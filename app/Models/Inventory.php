ec085883ddeafe77d8cc336e84d72d63296a8946
Mark Joven Baleta <markjovenabaletawsi@gmail.com>
2025-05-11 04:21:54 +0800

    database

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
+    /** @use HasFactory<\Database\Factories\InventoryFactory> */
+    use HasFactory;
 
+     protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'sku',
+        'price',
+        'stock',
+        'discount',
+        'cost',
+        'image',
+        'status',
+    ];
 }
